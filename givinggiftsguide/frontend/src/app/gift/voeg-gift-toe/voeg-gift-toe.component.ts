ff68b40612cb40d626ffec2de545b2d22daab39e
BramDC3 <bram.deconinck.y7538@student.hogent.be>
2018-04-25 09:52:57 +0200

    Opmaak en validatie van voeg-gift-toe afgewerkt

--- givinggiftsguide/frontend/src/app/gift/voeg-gift-toe/voeg-gift-toe.component.ts
+++ givinggiftsguide/frontend/src/app/gift/voeg-gift-toe/voeg-gift-toe.component.ts
@@ -25,29 +25,29 @@ export class VoegGiftToeComponent implements OnInit {
 
   ngOnInit() {
     this.gift = this.fb.group({
-      naam: ['Naam van de gift', [Validators.required, Validators.minLength(3)]],
-      beschrijving: ['Beschrijving van de gift', [Validators.required, Validators.minLength(10)]],
-      prijs: [123, [Validators.required, Validators.min(1), Validators.max(5000)]],
-      categorieen: this.fb.array([ this.maakCategorieen() ])
+      naam: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      beschrijving: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]],
+      prijs: ['', [Validators.required, Validators.min(1), Validators.max(9999)]],
+      categorieen: this.fb.array([this.maakCategorieen()])
     });
 
     this.categorieen.valueChanges
       .pipe(debounceTime(400), distinctUntilChanged())
       .subscribe(catList => {
         const lastElement = catList[catList.length - 1];
-        if ( lastElement.categorienaam &&
-          lastElement.categorienaam.length > 2 ) 
-          { this.categorieen.push(this.maakCategorieen());
-          } else if (catList.length >= 2) {
-            const secondToLast = catList[catList.length - 2];
-            if (
-              !lastElement.categorienaam &&
-              (!secondToLast.categorienaam ||
-                secondToLast.categorienaam.length < 2)
-            ) {
-              this.categorieen.removeAt(this.categorieen.length - 1);
-            }
+        if (lastElement.categorienaam &&
+          lastElement.categorienaam.length > 2) {
+          this.categorieen.push(this.maakCategorieen());
+        } else if (catList.length >= 2) {
+          const secondToLast = catList[catList.length - 2];
+          if (
+            !lastElement.categorienaam &&
+            (!secondToLast.categorienaam ||
+              secondToLast.categorienaam.length < 2)
+          ) {
+            this.categorieen.removeAt(this.categorieen.length - 1);
           }
+        }
       });
   }
 
@@ -59,17 +59,20 @@ export class VoegGiftToeComponent implements OnInit {
   }
 
   onSubmit() {
-    const gift = new Gift(this.gift.value.naam, this.gift.value.beschrijving, this.gift.value.prijs);
-    for (const cat of this.gift.value.categorieen) {
-      if (cat.categorienaam.length > 2) {
-        gift.voegCategorieToe(new Categorie(cat.categorienaam, cat.soort));
+    if (this.gift.value.naam != null && this.gift.value.naam != "" && this.gift.value.beschrijving != null && this.gift.value.beschrijving != "" && this.gift.value.prijs != null && this.gift.value.prijs != "" &&
+        this.gift.value.naam.length >= 3 && this.gift.value.naam.length <= 50 && this.gift.value.beschrijving.length >= 10 && this.gift.value.beschrijving.length <= 500 && this.gift.value.prijs >= 1 && this.gift.value.prijs <= 9999) {
+      const gift = new Gift(this.gift.value.naam, this.gift.value.beschrijving, this.gift.value.prijs);
+      for (const cat of this.gift.value.categorieen) {
+        if (cat.categorienaam.length > 2) {
+          gift.voegCategorieToe(new Categorie(cat.categorienaam, cat.soort));
+        }
       }
+      this._giftDataService.voegNieuweGiftToe(gift).subscribe(
+        item => { },
+        (error: HttpErrorResponse) => {
+          this.errorMsg = `Error ${error.status} bij het toevoegen van de gift ${gift.naam}: ${error.error}`;
+        }
+      );
     }
-    this._giftDataService.voegNieuweGiftToe(gift).subscribe(
-      item => {},
-      (error: HttpErrorResponse) => {
-        this.errorMsg = `Error ${error.status} bij het toevoegen van de gift ${gift.naam}: ${error.error}`;
-      }
-    );
   }
 }
