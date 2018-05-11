6115f8ec6d0c4f9a9926780697a36a03b3a44f2f
BramDC3 <bram.deconinck.y7538@student.hogent.be>
2018-05-11 23:39:44 +0200

    aanpassingen voor heroku

--- givinggiftsguide/frontend/src/environments/environment.prod.ts
+++ givinggiftsguide/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  token: '712fb31c683a4958bf79daeaf2f23c58'
 };
 
 export const BACKEND_URL = "https://givinggiftsguide-backend.herokuapp.com";
